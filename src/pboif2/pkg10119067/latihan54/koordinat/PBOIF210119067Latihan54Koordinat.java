e144e302045648329d049b8263dec507f4fe8f6b
User <User@DESKTOP-6N6EHH6>
2020-11-21 22:37:34 +0700

    PBOIF2-10119067

---
+++ src/pboif2/pkg10119067/latihan54/koordinat/PBOIF210119067Latihan54Koordinat.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119067.latihan54.koordinat;
+
+/**
+ *
+ * @author Danhago
+ * NAMA     : Andreas Danhago P.I
+ * KELAS    : IF2
+ * NIM      : 10119067
+ * Deskripsi: Koordinat
+ */
+public class PBOIF210119067Latihan54Koordinat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        WarnaKoordinat koordinat = new WarnaKoordinat(10,4,"Jingga");
+        
+        System.out.println("Warna Koordinat : " + koordinat.getNamaWarna());
+        System.out.println("Koordinat Sumbu x : " + koordinat.getX() + ", y : " + koordinat.getY());
+    }
+    
+}
